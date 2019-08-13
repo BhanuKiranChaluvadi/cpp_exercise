3be30304dffa28652588333dcc441b4013541c4f
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2019-08-13 06:43:50 +0200

    Added method logic

--- udacity/new_delete_binary_tree_exercise.cpp
+++ udacity/new_delete_binary_tree_exercise.cpp
@@ -1,18 +1,20 @@
 // RUN: g++ -std=c++17 -Wall -Wextra -Wpedantic -Wunused new_delete_binary_tree_exercise.cpp && ./a.out
 // cppcheck --enable=all ./new_delete_binary_tree_exercise.cpp
 
-#include<iostream>
+#include <iostream>
 
-using namespace std;
+class binary_tree
+{
 
-class Binary_tree {
-
-    struct Node {
+private:
+    struct node
+    {
         int data;
-        Node *left;
-        Node *right;
-        Node(int data, Node *left, Node *right): data(data), left(left), right(right) {}
-        ~Node() {
+        node *left;
+        node *right;
+        node(int _data, node *_left, node *_right) : data(_data), left(_left), right(_right) {}
+        ~node()
+        {
             if (left)
                 delete left;
             if (right)
@@ -20,34 +22,93 @@ class Binary_tree {
         }
     };
 
-    Node *root;
+    node *root;
 
-    void add(int val, Node *ptr) {
-        // TODO:
+    void add(int val, node *ptr)
+    {
+        if (val > ptr->data)
+        {
+            if (ptr->left != NULL)
+                add(val, ptr->left);
+            else
+                ptr->left = new node(val, NULL, NULL);
+        }
+        else if (val <= ptr->data)
+        {
+            if (ptr->right != NULL)
+                add(val, ptr->right);
+            else
+                ptr->right = new node(val, NULL, NULL);
+        }
     }
-    bool search(int item,  Node *ptr) {
-        // TODO:
-        return true;
+    bool search(int item, node *ptr)
+    {
+        /* 
+        if(!ptr->left && !ptr->right){
+            if(ptr->data == item){
+                return true;
+            }
+            return false;
+        }
+        if(ptr->data < item && ptr->left){
+            search(item, ptr->left);
+        }
+        else if(ptr->data > item && ptr->right){
+            search(item, ptr->right);
+        }
+        */
+
+        if (item == ptr->data)
+            return true;
+        else
+        {
+            if (ptr->left != NULL && item > ptr->data)
+                return search(item, ptr->left);
+            else if (ptr->right != NULL && item < ptr->data)
+                return search(item, ptr->right);
+            else
+                return false;
+        }
     }
 
 public:
-    Binary_tree() {}
-    ~Binary_tree() {
-         delete Binary_tree::root;
+    binary_tree(int data)
+    {
+        binary_tree::root = new node(data, NULL, NULL);
     }
-    void addData(int new_data) {
-        Binary_tree::add(new_data, Binary_tree::root);
+    ~binary_tree()
+    {
+        delete binary_tree::root;
     }
-    bool searchItem(int item) {
-        return Binary_tree::search(item, Binary_tree::root);
+    void addData(int new_data)
+    {
+        binary_tree::add(new_data, binary_tree::root);
     }
-
-
+    bool searchItem(int item)
+    {
+        return binary_tree::search(item, binary_tree::root);
+    }
 };
 
+/*
 
-int main() {
+                                    2
+                            10              1
+                        20      N       N       0
+                    N       11
+*/
 
+int main()
+{
+    binary_tree tree(2);
+    tree.addData(10);
+    tree.addData(1);
+    tree.addData(20);
+    tree.addData(0);
 
+    std::cout << tree.searchItem(0) << std::endl;
+    std::cout << tree.searchItem(9) << std::endl;
+    tree.addData(11);
+    std::cout << tree.searchItem(5) << std::endl;
     return 0;
 }
