a973c6c826553570fd51893d3c16948509e3d7a7
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2019-09-08 11:21:42 +0200

    added comments.

--- udacity/threads_avoidDeadLock_3.cpp
+++ udacity/threads_avoidDeadLock_3.cpp
@@ -17,14 +17,14 @@ std::mutex mutex1, mutex2;
 
 void ThreadA() {
     // Ensure that locks are always exeuted in the same order.
-    std::lock(mutex1, mutex2);
+    std::lock(mutex1, mutex2);      // Just locks them ...but will not unlock and hence lock_guard
     std::lock_guard<std::mutex> lock2(mutex2, std::adopt_lock);
     std::cout << "Thread A"<< std::endl;
     std::lock_guard<std::mutex> lock1(mutex1, std::adopt_lock);
 }
 
 void ThreadB() {
-    std::lock(mutex1, mutex2);
+    std::lock(mutex1, mutex2);       // Just locks them ...but will not unlock and hence lock_guard
     std::lock_guard<std::mutex> lock1(mutex1, std::adopt_lock);
     std::cout << "Thread B"<< std::endl;
     std::lock_guard<std::mutex> lock2(mutex2, std::adopt_lock);
